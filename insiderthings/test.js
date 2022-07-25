8a05c0ffcf4d009ea8117b990c7dd4e82dc2d312
Docoo <alexdufc@gmail.com>
2022-07-25 13:59:52 +0300

    implemented test for logging to discord

--- insiderthings/test.js
+++ insiderthings/test.js
@@ -12,10 +12,25 @@ module.exports={
         // message.channel.send("Test complete!");
         //console.log(new Date().getDay());
 
-        const Discord = require('Discord.js')
-        files = []
-        files.push(new Discord.MessageAttachment(`./assets/BD.png`))
-        const newEmbed = new Discord.MessageEmbed().setImage('attachment://BD.png')
-        message.channel.send({embeds: [newEmbed], files: files})
+        switch(args){
+            case "1":
+                const Discord = require('Discord.js')
+                files = []
+                files.push(new Discord.MessageAttachment(`./assets/BD.png`))
+                const newEmbed = new Discord.MessageEmbed().setImage('attachment://BD.png')
+                message.channel.send({embeds: [newEmbed], files: files})
+                break;
+            case "2":
+                const msg = "This is a test message to log to the log channel"
+                logguild = bot.guilds.resolve('247831161013796865');
+                logchannel = logguild.channels.resolve('642485408722190336');
+                logchannel.send(`[${date2.getHours()}:${date2.getMinutes()}:${date2.getSeconds()}]\n\`\`\`${msg}\`\`\``);
+                break;
+            default:
+                return message.reply("You did not specify a valid test case")
+                break;
+        }
+        message.reply("Test executed successfully!")
+
     }
 }
